eb15fb72ddfe7af5ef22d5626319273614e7383b
ayaz1o1 <ayaz_ansari1983@yahoo.com>
2020-06-29 00:00:43 +0500

    Update app.js
    just functionsa added for input

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 
+var value1 ;
+var value2 ;
+var operator ;
+function getNumber(num){
+   var vals = num; 
+ var valu = document.getElementById('res');
+// var result = document.getElementById(num);
+// result = result.value + ; 
+valu.value = (valu.value) + vals ;
+
+}
+
+function clr(){
+// alert("hi");
+   var result = document.getElementById('res');
+    result.value = "";
+}
+
+// function one(){
+//    var valu = 1;
+// //    valu.value =  
+//     var vals = document.getElementById('res');
+//    vals.value = (vals.value) + valu ;
+//    }
+
+//    function plus(){
+//     // var valu = "+";
+//     var valu = document.getElementById('res');
+
+//    valu.value = ((valu.value) + "+");
+//    var valu1 = valu.value;
+//    valu1 = +
+//    }
+
+//    function equal(){
+    
+//     var valu = document.getElementById('res');
+//    valu.value = +(valu.value);
+// alert(typeof(valu.value));
+//    }
